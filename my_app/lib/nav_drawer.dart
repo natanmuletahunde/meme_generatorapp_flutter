654e48a2963c4d7ff2448f99c10e155bfc02bd04
pivnati <natanmuleta911@gmail.com>
2024-08-09 16:55:19 -0700

    adding the navdrawer in the appbar

---
+++ my_app/lib/nav_drawer.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class NavDrawer extends StatefulWidget {
+  const NavDrawer({super.key});
+
+  @override
+  State<NavDrawer> createState() => _NavDrawerState();
+}
+
+class _NavDrawerState extends State<NavDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
